028eeb4f556c4772cc285774e54606d7dee3a83e
theCat89 <kmi_89@mail.ru>
2015-07-02 20:18:28 +0300

    Мелкие правки, дописан класс User

--- Shad/src/Test.java
+++ Shad/src/Test.java
@@ -2,4 +2,5 @@
  * Created by Dartaan on 01.07.2015.
  */
 public class Test {
+    //todo smth with this
 }
--- Shad/src/User.java
+++ Shad/src/User.java
@@ -1,5 +1,13 @@
 /**
  * Created by Dartaan on 01.07.2015.
+ * Класс для описания пользователя в системе
+ * разграничивает доступ к функциям на сервере
+ * и доступ к формам в пользовательском интерфейсе
  */
 public class User {
+    public int id; // идентификатор доступа в системе
+    public String surname;  // Фамилия
+    public String name;     // Имя
+    public String secondName;// Отчество
+    public boolean isLecturer; // флаг является ли преподавателем!
 }
