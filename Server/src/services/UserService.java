345ec64eca0bf42a807e214cef9e76a12d786637
lazydolt <maxfootballist@mail.ru>
2015-07-04 21:18:34 +0400

    Набросок функция для реализации регистрации, авторизации и вывода списка студентов

---
+++ Server/src/services/UserService.java
@@ -0,0 +1,25 @@
+package services;
+
+/**
+ * Сервис по работе с пользователем
+ */
+public class UserService {
+    /*
+    регистрация пользователя
+     */
+    public boolean RegUser(){
+        return false;
+    }
+    /*
+    авторизация пользователя
+     */
+    public boolean AuthUser(){
+        return false;
+    }
+    /*
+    Получение списка студентов в группе
+     */
+    public boolean getStudents(){
+        return false;
+    }
+}
