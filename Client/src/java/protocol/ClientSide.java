c811790600fb6f1691ed52ee26b4c32691a3514f
Михаил Федин(1) <vbif.32@yandex.ru>
2015-07-10 23:25:56 +0300

    Реформатирование кода, оптимизация импортов, добавление комментариев, удаление readme в пакете протокола

--- Client/src/java/protocol/ClientSide.java
+++ Client/src/java/protocol/ClientSide.java
@@ -1,10 +1,11 @@
 package protocol;
 
-import query.*;
+import query.Queries;
+import query.Query;
 import reply.*;
+
 import javax.json.*;
 import javax.json.stream.JsonParser;
-
 import java.io.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
@@ -19,7 +20,8 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * Created by Lognir on 04.07.2015.
+ * Класс для описания протокола связи клиента и сервера
+ * Ответственный: Александр Шведов
  */
 public class ClientSide {
 
@@ -36,39 +38,42 @@ public class ClientSide {
         Stack<List> arrayStack = new Stack<>();
         boolean arrayValue = false;
         int fieldsNum = 0;
-        if(jp.hasNext()) jp.next();
+        if (jp.hasNext()) jp.next();
 
         while (jp.hasNext()) {
             JsonParser.Event event = jp.next();
             switch (event) {
                 case KEY_NAME:
                     currentField = currentObject.getClass().getField(jp.getString());
-                    if(currentObject.equals(obj)) fieldsNum++; break;
+                    if (currentObject.equals(obj)) fieldsNum++;
+                    break;
                 case VALUE_STRING:
-                    if(arrayValue) {
+                    if (arrayValue) {
                         currentArray.add(currentType.getConstructor(String.class).newInstance(jp.getString()));
-                    }
-                    else if(!currentField.getType().equals(File.class))
+                    } else if (!currentField.getType().equals(File.class))
                         currentField.set(currentObject, currentField.getType().getConstructor(String.class).newInstance(jp.getString()));
                     else {
                         currentField.set(currentObject, File.class.getConstructor(File.class, String.class).newInstance(PATH, jp.getString()));
                         files.add((File) currentField.get(currentObject));
-                    } break;
+                    }
+                    break;
                 case VALUE_NULL:
-                    if(arrayValue) currentArray.add(null);
-                    else currentField.set(currentObject, null); break;
+                    if (arrayValue) currentArray.add(null);
+                    else currentField.set(currentObject, null);
+                    break;
                 case START_ARRAY:
-                    if(currentArray != null) {
+                    if (currentArray != null) {
                         arrayStack.push(currentArray);
                         typeStack.push(currentType);
                     }
-                    currentArray = (List)currentField.getType().getConstructor().newInstance();
+                    currentArray = (List) currentField.getType().getConstructor().newInstance();
                     currentField.set(currentObject, currentArray);
-                    currentType = (Class)((ParameterizedType)currentField.getGenericType()).getActualTypeArguments()[0];
-                    arrayValue = true; break;
+                    currentType = (Class) ((ParameterizedType) currentField.getGenericType()).getActualTypeArguments()[0];
+                    arrayValue = true;
+                    break;
                 case START_OBJECT:
                     objectStack.push(currentObject);
-                    if(currentArray != null) {
+                    if (currentArray != null) {
                         arrayValue = false;
                         currentObject = currentType.getConstructor().newInstance();
                         currentArray.add(currentObject);
@@ -76,14 +81,17 @@ public class ClientSide {
                         Object tmp = currentField.getType().getConstructor().newInstance();
                         currentField.set(currentObject, tmp);
                         currentObject = tmp;
-                    } break;
+                    }
+                    break;
                 case END_ARRAY:
-                    if(typeStack.size() > 0) currentType = typeStack.pop();
-                    if(arrayStack.size() > 0) currentArray = arrayStack.pop();
-                    arrayValue = false; break;
+                    if (typeStack.size() > 0) currentType = typeStack.pop();
+                    if (arrayStack.size() > 0) currentArray = arrayStack.pop();
+                    arrayValue = false;
+                    break;
                 case END_OBJECT:
-                    if(objectStack.size() > 0) currentObject = objectStack.pop();
-                    if(currentArray != null) arrayValue = true; break;
+                    if (objectStack.size() > 0) currentObject = objectStack.pop();
+                    if (currentArray != null) arrayValue = true;
+                    break;
             }
         }
         jp.close();
@@ -92,34 +100,33 @@ public class ClientSide {
 
     public static JsonObjectBuilder getJasonObj(Object obj, ArrayList<File> files) throws IllegalAccessException {
         JsonObjectBuilder job = Json.createObjectBuilder();
-        if(obj == null) return job;
+        if (obj == null) return job;
         Field[] fields = obj.getClass().getFields();
-        for(Field f : fields) {
-            if(f.get(obj) == null)
+        for (Field f : fields) {
+            if (f.get(obj) == null)
                 job.add(f.getName(), JsonValue.NULL);
-            else if(f.getType().equals(File.class)) {
-                File file = (File)f.get(obj);
+            else if (f.getType().equals(File.class)) {
+                File file = (File) f.get(obj);
                 job.add(f.getName(), file.getName());
                 files.add(file);
-            }
-            else if(List.class.isAssignableFrom(f.getType())) {
+            } else if (List.class.isAssignableFrom(f.getType())) {
                 job.add(f.getName(), getJasonArr(obj, f, files));
-            }
-            else {
+            } else {
                 job.add(f.getName(), (f.get(obj)).toString());
             }
         }
         return job;
     }
+
     public static JsonArrayBuilder getJasonArr(Object obj, Field field, ArrayList<File> files) throws IllegalAccessException {
         JsonArrayBuilder jab = Json.createArrayBuilder();
         boolean objFlag = true;
-        List list = (List)field.get(obj);
-        Class type = (Class)((ParameterizedType)field.getGenericType()).getActualTypeArguments()[0];
-        if(type.equals(String.class) || type.equals(Integer.class) || type.equals(Boolean.class)) objFlag = false;
-        for(Object o : list) {
-            if(o == null) jab.add(JsonValue.NULL);
-            else if(objFlag) jab.add(getJasonObj(o, files));
+        List list = (List) field.get(obj);
+        Class type = (Class) ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0];
+        if (type.equals(String.class) || type.equals(Integer.class) || type.equals(Boolean.class)) objFlag = false;
+        for (Object o : list) {
+            if (o == null) jab.add(JsonValue.NULL);
+            else if (objFlag) jab.add(getJasonObj(o, files));
             else jab.add(o.toString());
         }
         return jab;
@@ -134,42 +141,40 @@ public class ClientSide {
             JsonObject jmsg = getJasonObj(query, files).build();
             byte[] byteArr = jmsg.toString().getBytes();
             byte[] len = ByteBuffer.allocate(4).putInt(byteArr.length).array();
-            byte[] resMsg = new byte[byteArr.length+6];
+            byte[] resMsg = new byte[byteArr.length + 6];
             System.arraycopy(len, 0, resMsg, 2, 4);
             System.arraycopy(byteArr, 0, resMsg, 6, byteArr.length);
             resMsg[0] = 1;
-            resMsg[1] = (byte)type.ordinal();
+            resMsg[1] = (byte) type.ordinal();
 
             int answer = 0;
             do {
-                if(answer == -1) throw new IOException("Connection lost.");
-                else if(answer == 63) throw new WrongDataException("Server denied request.");
+                if (answer == -1) throw new IOException("Connection lost.");
+                else if (answer == 63) throw new WrongDataException("Server denied request.");
                 out.write(resMsg);
                 out.flush();
-            } while((answer = in.read()) != 33);
+            } while ((answer = in.read()) != 33);
 
             MessageDigest md = MessageDigest.getInstance("MD5");
-            if(files.size() > 0) {
+            if (files.size() > 0) {
                 DigestInputStream fs;
                 int fLen, numRead;
                 byte[] resHead = new byte[6];
                 resHead[0] = 1;
                 resHead[1] = 31;
                 answer = 0;
-                for(File fl : files) {
-                    if(fl.length() > Integer.MAX_VALUE) throw new FileReadingException("File is too large.");
-                    fLen = (int)fl.length();
-                    resMsg = new byte[fLen+22];
+                for (File fl : files) {
+                    if (fl.length() > Integer.MAX_VALUE) throw new FileReadingException("File is too large.");
+                    fLen = (int) fl.length();
+                    resMsg = new byte[fLen + 22];
                     try {
                         fs = new DigestInputStream(new FileInputStream(fl), md);
                         numRead = fs.read(resMsg, 22, fLen);
                         fs.close();
-                        if(numRead != fLen) throw new FileReadingException("File reading was not complete.");
-                    }
-                    catch(FileNotFoundException e) {
+                        if (numRead != fLen) throw new FileReadingException("File reading was not complete.");
+                    } catch (FileNotFoundException e) {
                         throw new FileReadingException("File was not found.");
-                    }
-                    catch(IOException iof) {
+                    } catch (IOException iof) {
                         throw new FileReadingException("File could not be read.");
                     }
                     len = ByteBuffer.allocate(4).putInt(fLen).array();
@@ -179,11 +184,11 @@ public class ClientSide {
                     md.reset();
 
                     do {
-                        if(answer == -1) throw new IOException("Connection lost.");
-                        else if(answer == 63) throw new WrongDataException("Server denied files.");
+                        if (answer == -1) throw new IOException("Connection lost.");
+                        else if (answer == 63) throw new WrongDataException("Server denied files.");
                         out.write(resMsg);
                         out.flush();
-                    } while((answer = in.read()) != 33);
+                    } while ((answer = in.read()) != 33);
                 }
                 files.clear();
             }
@@ -201,9 +206,9 @@ public class ClientSide {
             byte[] answerData;
             int bytesRead;
 
-            while(!correct) {
+            while (!correct) {
                 tryCount++;
-                if(tryCount > 3) {
+                if (tryCount > 3) {
                     throw new ServerBadDataException("Bad data from server.");
                 }
                 errCount = 0;
@@ -219,17 +224,17 @@ public class ClientSide {
                 answerType = in.read();
                 answer = in.read(answerLen, 0, 4);
                 ansLen = ByteBuffer.wrap(answerLen).getInt();
-                if(answer != 4 || ansLen < 0) {
+                if (answer != 4 || ansLen < 0) {
                     out.write(23);
                     out.flush();
                     continue;
                 }
                 answerData = new byte[ansLen];
                 answer = 0;
-                while(answer != ansLen) {
+                while (answer != ansLen) {
                     bytesRead = in.read(answerData, answer, ansLen - answer);
                     answer += bytesRead;
-                    if(bytesRead <= 0) break;
+                    if (bytesRead <= 0) break;
                 }
                 if (answer != ansLen) {
                     out.write(23);
@@ -239,54 +244,62 @@ public class ClientSide {
 
                 String rawData = new String(answerData);
 
-                switch(answerType) {
+                switch (answerType) {
                     case 0:
-                        qr = new User(); break;
+                        qr = new User();
+                        break;
                     case 1:
-                        qr = new QueryError(); break;
+                        qr = new QueryError();
+                        break;
                     case 2:
-                        qr = new Test(); break;
+                        qr = new Test();
+                        break;
                     case 3:
-                        qr = new TestResult(); break;
+                        qr = new TestResult();
+                        break;
                     case 4:
-                        qr = new TestUploading(); break;
+                        qr = new TestUploading();
+                        break;
                     case 5:
-                        qr = new Stats(); break;
+                        qr = new Stats();
+                        break;
                     case 6:
-                        qr = new Registration(); break;
+                        qr = new Registration();
+                        break;
                     default:
-                        out.write(23); out.flush(); continue;
+                        out.write(23);
+                        out.flush();
+                        continue;
                 }
 
                 int fieldsNum;
                 try {
                     fieldsNum = parseJsonString(qr, rawData, files);
-                } catch(ReflectiveOperationException roe) {
+                } catch (ReflectiveOperationException roe) {
                     out.write(23);
                     out.flush();
                     continue;
                 }
-                if(fieldsNum != qr.getClass().getFields().length) {
+                if (fieldsNum != qr.getClass().getFields().length) {
                     out.write(23);
                     out.flush();
-                }
-                else
+                } else
                     correct = true;
             }
             out.write(33);
 
             int i = 0;
             DigestOutputStream fos;
-            while((answer = in.read()) != 33) {
+            while ((answer = in.read()) != 33) {
                 if (answer == -1) throw new IOException("Connection lost.");
                 correct = false;
                 tryCount = 0;
-                while(!correct) {
+                while (!correct) {
                     tryCount++;
-                    if(tryCount > 3 || i >= files.size()) {
+                    if (tryCount > 3 || i >= files.size()) {
                         throw new ServerBadDataException("Bad data from server.");
                     }
-                    if(answer != 1) {
+                    if (answer != 1) {
                         errCount = 0;
                         while ((answer = in.read()) != 1) {
                             if (answer == -1) throw new IOException("Connection lost.");
@@ -307,10 +320,10 @@ public class ClientSide {
                     }
                     answerData = new byte[ansLen];
                     answer = 0;
-                    while(answer != ansLen) {
+                    while (answer != ansLen) {
                         bytesRead = in.read(answerData, answer, ansLen - answer);
                         answer += bytesRead;
-                        if(bytesRead <= 0) break;
+                        if (bytesRead <= 0) break;
                     }
                     if (answer != ansLen) {
                         out.write(23);
@@ -322,14 +335,12 @@ public class ClientSide {
                         fos.write(answerData);
                         fos.flush();
                         fos.close();
-                    }
-                    catch(FileNotFoundException e) {
+                    } catch (FileNotFoundException e) {
                         throw new FileReadingException("Couldn't make a file.");
-                    }
-                    catch(IOException iof) {
+                    } catch (IOException iof) {
                         throw new FileReadingException("Couldn't write into the file.");
                     }
-                    if(!Arrays.equals(md5data, md.digest())) {
+                    if (!Arrays.equals(md5data, md.digest())) {
                         out.write(23);
                         out.flush();
                         md.reset();
@@ -341,49 +352,42 @@ public class ClientSide {
                 }
                 i++;
             }
-            if(i != files.size()) {
+            if (i != files.size()) {
                 correct = false;
                 throw new ServerBadDataException("Bad data from server.");
             }
-        }
-        catch(IOException ioe) {
+        } catch (IOException ioe) {
             return new QueryError(ioe.getMessage());
-        }
-        catch(WrongDataException wde) {
+        } catch (WrongDataException wde) {
             try {
-                if((in.read()) == 1 && (in.read()) == 1) {
+                if ((in.read()) == 1 && (in.read()) == 1) {
                     byte[] answerLen = new byte[4];
-                    if(in.read(answerLen, 0, 4) != 4) throw new Exception();
+                    if (in.read(answerLen, 0, 4) != 4) throw new Exception();
                     int ansLen = ByteBuffer.wrap(answerLen).getInt();
                     byte[] answerData = new byte[ansLen];
-                    if(in.read(answerData, 0, ansLen) != ansLen) throw new Exception();
+                    if (in.read(answerData, 0, ansLen) != ansLen) throw new Exception();
                     String rawData = new String(answerData);
                     qr = new QueryError();
                     parseJsonString(qr, rawData, null);
                     return qr;
                 }
                 return new QueryError(wde.getMessage());
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 return new QueryError(e.getMessage());
             }
-        }
-        catch(FileReadingException|ServerBadDataException fre) {
+        } catch (FileReadingException | ServerBadDataException fre) {
             try {
                 out.write(63);
                 out.flush();
                 return new QueryError(fre.getMessage());
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 return new QueryError(e.getMessage());
             }
-        }
-        catch(IllegalAccessException|NoSuchAlgorithmException iae) {
+        } catch (IllegalAccessException | NoSuchAlgorithmException iae) {
             return new QueryError(iae.getMessage());
-        }
-        finally {
-            if(!correct)
-                for(File f : files)
+        } finally {
+            if (!correct)
+                for (File f : files)
                     f.delete();
         }
 
