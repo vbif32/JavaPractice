4d2823e37b4d0d2ffa9e5bdf9b1cbac383582e6c
pavlovq <pavlov.cpp@gmail.com>
2015-07-10 15:21:38 +0300

    Изменение StatsService под новый шед

--- Server/src/java/services/StatsService.java
+++ Server/src/java/services/StatsService.java
@@ -1,9 +1,11 @@
 package services;
 
 import managers.DataBase.DatabaseManager;
-import shed.QueryResult;
-import shed.query.StatsRequest;
-import shed.queryResult.Stats;
+import query.Query;
+import query.StatsRequest;
+import reply.Reply;
+import reply.Stats;
+
 
 /**
  * Created by Пользователь on 06.07.2015.
@@ -14,7 +16,7 @@ public class StatsService {
      * Запрос к менеджеру БД за данными о студентах с входящими параметрами: предмет и учебная группа
      * Возврат ArrayList студентов
      */
-    public static QueryResult getStats(StatsRequest statsRequest) {
+    public static Reply getStats(StatsRequest statsRequest) {
         DatabaseManager manager = new DatabaseManager();
         return new Stats(manager.GetStudentInfo(statsRequest.group, statsRequest.subject));
     }
