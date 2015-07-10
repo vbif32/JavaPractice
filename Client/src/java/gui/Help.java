11d327f53c4cd39c9714b215e0f0ad8eadf02a8f
Михаил Федин(1) <vbif.32@yandex.ru>
2015-07-10 19:23:40 +0300

    Удаление лишнего из класса вызова помощи

--- Client/src/java/gui/Help.java
+++ Client/src/java/gui/Help.java
@@ -3,22 +3,12 @@ package gui;
 import java.awt.*;
 import java.io.*;
 
-public class TestOpenHelp {
+public class Help {
 
     private static String res_name = "test.txt";
 
-    public static File streamIntoFile(InputStream input, String name) throws IOException {
-        byte[] buffer = new byte[input.available()];
-        input.read(buffer);
-        File targetFile = new File(name);
-        OutputStream out = new FileOutputStream(targetFile);
-        out.write(buffer);
-        out.close();
-        return targetFile;
-    }
-
     public static File resToFile(String res_name, String name) throws IOException {
-        InputStream input = TestOpenHelp.class.getResourceAsStream("/" + res_name);
+        InputStream input = Help.class.getResourceAsStream("/" + res_name);
         byte[] buffer = new byte[input.available()];
         input.read(buffer);
         File targetFile = new File(name);
@@ -33,9 +23,5 @@ public class TestOpenHelp {
         File help = resToFile(res_name, res_name);
         desktop.open(help);
         help.deleteOnExit();
-    }
-
-    public static void main(String[] args) throws IOException {
-        openHelp();
     }
 }
